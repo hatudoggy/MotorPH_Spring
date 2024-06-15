8ebb658d09e0c32675b1e71c0bba6fc4acc8fc24
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2024-06-15 22:53:08 +0800

    Contact

---
+++ backend/src/main/java/com/motorph/ems/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.motorph.ems.repository;
+
+import com.motorph.ems.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
