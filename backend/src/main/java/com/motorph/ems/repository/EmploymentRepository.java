afcbfd3ca291204f1d94d1bed0a85a61e5e7b060
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2024-06-24 03:43:27 +0800

    Employment

--- backend/src/main/java/com/motorph/ems/repository/EmploymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.motorph.ems.repository;
-
-import com.motorph.ems.model.Employment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmploymentRepository extends
-        JpaRepository<Employment, Long> {
-}
